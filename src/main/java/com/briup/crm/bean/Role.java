f23716658b4d50ea4f0df43241c3be31ad6f19b3
kj <jiayeEZ@aliyun.com>
2020-03-26 14:25:51 +0800

    2020/3/26 crm_version_1.0

---
+++ src/main/java/com/briup/crm/bean/Role.java
@@ -0,0 +1,79 @@
+package com.briup.crm.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Role implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+
+	private String name;
+
+	private String description;
+
+	private Integer flag;
+
+	
+
+	
+	public Role() {
+	}
+	
+	public Role(Integer id) {
+		super();
+		this.id = id;
+	}
+
+
+	/*public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}*/
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getFlag() {
+		return flag;
+	}
+
+	public void setFlag(Integer flag) {
+		this.flag = flag;
+	}
+
+	@Override
+	public String toString() {
+		return "Role{" + "id=" + id + ", name=" + name + ", description=" + description + ", flag=" + flag + "}";
+	}
+
+}
